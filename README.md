4c3a09343f345df5be0d39dcd4dfdb28d41d5ed1
jeroentvb <jeroentvb@hotmail.com>
2019-08-02 21:16:19 +0200

    Improved error checking

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Tiny package to trigger an IFTTT event using the [webhooks channel](https://iftt
 * [Usage](#usage)
 * [Methods](#methods)
   * [Post()](#post)
+* [Example](#example)
 
 ## Installation
 ```sh
@@ -28,7 +29,25 @@ A list of methods that `IFTTT` contains
 Sends a post request to the webhooks channel to trigger an ifttt event.
 ```js
 ifttt.post(eventName, value)
+  .then(res => console.log(res))
+  .catch(err => console.error(err))
 ```
 
 `eventName` is the name of the event you should have created on the webhooks channel.  
 `value` should be an array containing at most 3 values. These can be used in the action you attatch to the event when it's triggered, e.g. a notification.
+
+The function returns the response object if the post was succesful. If there was some kind of error it returns an error object.
+
+## Example
+```js
+const IFTTT = require('ifttt-webhooks-channel')
+const ifttt = new IFTTT('your_webhooks_channel_key')
+
+ifttt.post('event_name', [
+  'value 1',
+  'item 2',
+  'string 3'
+])
+  .then(res => console.log(res))
+  .catch(err => console.error(err))
+```
--- ifttt.js
+++ ifttt.js
@@ -6,34 +6,32 @@ class IFTTT {
     this.key = key
   }
 
-  post (eventName, value) {
-    return new Promise(async (resolve, reject) => {
-      if (!eventName) return reject(new Error('No event name given'))
+  async post (eventName, value) {
+    if (!eventName) return new Error('No event name given.')
+    if (value && !Array.isArray(value)) return new TypeError('The values must be in an array.')
 
-      try {
-        const res = await fetch(`https://maker.ifttt.com/trigger/${eventName}/with/key/${this.key}`, {
-          method: 'post',
-          body: value
-            ? JSON.stringify({
-              value1: value[0],
-              value2: value[1],
-              value3: value[2]
-            }) : '',
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        })
-
-        if (res.status !== 200) {
-          reject(new Error(`POST request failed with the following code: ${res.status}, ${res.statusText}`))
-          return
+    try {
+      const res = await fetch(`https://maker.ifttt.com/trigger/${eventName}/with/key/${this.key}`, {
+        method: 'post',
+        body: value
+          ? JSON.stringify({
+            value1: value[0],
+            value2: value[1],
+            value3: value[2]
+          }) : '',
+        headers: {
+          'Content-Type': 'application/json'
         }
+      })
 
-        resolve(res)
-      } catch (err) {
-        reject(err)
+      if (res.status !== 200) {
+        return new Error(`POST request failed with the following code: ${res.status}, ${res.statusText}`)
       }
-    })
+
+      return res
+    } catch (err) {
+      return err
+    }
   }
 }
 
