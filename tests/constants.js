0def77eaa65d71f61743025fffeae425b8aaf1a2
jeroentvb <36192730+jeroentvb@users.noreply.github.com>
2019-12-04 21:02:35 +0100

    Added tests for the IFTTT class

---
+++ tests/constants.js
@@ -0,0 +1,14 @@
+const API_URL = 'https://maker.ifttt.com/trigger/test/with/key/test_key'
+
+const REQUEST_OPTIONS = {
+  method: 'post',
+  body: '',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}
+
+module.exports = {
+  API_URL,
+  REQUEST_OPTIONS
+}
